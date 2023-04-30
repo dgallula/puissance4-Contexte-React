08da053686a3411bc707a3f3b15ec2c621ceb4c4
dgallula <dgallua@gmail.com>
2023-04-30 18:02:26 +0200

    etape4 init

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,12 @@
-import Fastify from "fastify"
+ import Fastify from "fastify"
+import {v4} from 'uuid'
 
 const fastify = Fastify({logger:true})
+
+fastify.post('/api/players', (req,res)=> {
+   const playerId= v4()
+   console.log({playerId})
+})
 
 fastify.listen({port:8000}).catch((err)=> {
   fastify.log.error(err)
